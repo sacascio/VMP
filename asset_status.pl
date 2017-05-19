9ad1b3eb2d58f2fee4182324ab0499baf819edb4
Cascio, Salvatore (sacascio) <sacascio@cisco.com>
2017-05-19 15:26:43 -0400

    version 2

--- asset_status.pl
+++ asset_status.pl
@@ -1,13 +1,13 @@
 #!/usr/bin/perl -w
 
 use strict;
-
 `./getsrc.pl`;
 
 my $callsign;
 my $res;
 my $detail;
-
+my $sourceip;
+my $mcip;
 # Parse out callsigns from asset_list
 #
 
@@ -19,13 +19,24 @@ while(<FILE>) {
 	chomp($_);
 	$_ =~ m/(.*),.*,.*/;
 	$callsign = $1;
-	$res = `curl -H "Authorization: Bearer c49d2ad386d45c41e5c1ca2bbfe531dab7136601d3cc01e3434b97b965118ac2" -ks https://service-mgr.mos.hcvlny.cv.net:7001/v1/assetWorkflows/live/assets/$callsign`;
+	#$res = `curl -H "Authorization: Bearer c49d2ad386d45c41e5c1ca2bbfe531dab7136601d3cc01e3434b97b965118ac2" -ks https://service-mgr.mos.hcvlny.cv.net:7001/v1/assetWorkflows/live/assets/$callsign`;
+	$res = `curl -H "Authorization: Bearer c49d2ad386d45c41e5c1ca2bbfe531dab7136601d3cc01e3434b97b965118ac2" -ks https://10.249.35.221:7001/v1/assetWorkflows/live/assets/$callsign`;
+	$res =~ m/.*"sourceUrl":"udp:\/\/.*:\d+","sourceIp":"(\d+\.\d+\.\d+\.\d+)",".*/;
+	$sourceip = $1;
+
+	$res =~ m/.*"sourceUrl":"udp:\/\/(.*):\d+","sourceIp.*/;
+	$mcip = $1;
+
 	$res =~ m/.*captureStatus(.*)/;
 	$detail = $1;
 	$detail =~ s/\":\[\{\"instanceId\":\"instance0\",\"state\"://g;
-	$detail = parse_log($detail);
-	print OUT "$callsign,Fail,$detail\n";
- 
+	($detail,$sourceip,$mcip) = parse_log($detail);
+
+	if ( $detail eq "CAPTURING" ) {
+		print OUT "$callsign,Pass,$detail,$sourceip,$mcip\n";
+	} else {
+		print OUT "$callsign,Fail,$detail,$sourceip,$mcip\n";
+	}
 
 }
 
@@ -43,8 +54,9 @@ my $br;
 
 		chomp($raw);
 
+		
 		if ( $raw =~ /CAPTURING/) {
-			return "CAPTURING";
+			return ("CAPTURING",$sourceip,$mcip);
 		}
 
 		my @tmp = split(/,/, $raw);
@@ -78,7 +90,7 @@ my $br;
 				$final = $message;
 			}
 
-			return $final;
+			return ($final,$sourceip,$mcip);
 
 }
 
