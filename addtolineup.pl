9c050884f338fcda9105cdca5711bd2e714b2662
Cascio, Salvatore (sacascio) <sacascio@cisco.com>
2017-06-05 12:38:33 -0400

    SDL Add to channel lineup

--- addtolineup.pl
+++ addtolineup.pl
@@ -1,18 +1,33 @@
 #!/usr/bin/perl -w
 #
+use Getopt::Std;
 use strict;
+
 my $str;
+my $sitename;
+my $domain;
+my $sm;
+
+getopts('hn:', \%opts);
+
+usage() if ( ! %opts );
+usage() if ( ! $opts{n} || !$opts{d} );
+usage() if ( $opts{h} );
+
+$sitename = $opts{n};
+$domain   = $opts{d};
+$sm = 'service-mgr.' . $domain;
 
 open(FILE,"callsigns") or die "Can't open callsigns\n";
 
 $str = <<EOF;
 {
-	"id": "smtenant_0.smchannellineup.Hicksville",
-	"name": "Hicksville",
+	"id": "smtenant_0.smchannellineup.$sitename",
+	"name": "$sitename",
 	"type": "channellineups",
-	"externalId": "/v2/channellineups/Hicksville",
+	"externalId": "/v2/channellineups/$sitename",
 	"properties": {
-		"description": "Hicksville Lineup",
+		"description": "$sitename Lineup",
 		"sourcesRef": [
 EOF
  
@@ -48,6 +63,6 @@ open(BUILD,">build") or die "Can't open build\n";
 print BUILD "$str\n";
 close BUILD;
 # Token following the word Bearer comes from the PAM in /etc/opt/cisco/mos/public/token.json
-`curl -H "Authorization: Bearer c49d2ad386d45c41e5c1ca2bbfe531dab7136601d3cc01e3434b97b965118ac2" -ks https://service-mgr.mos.hcvlny.cv.net:8043/v2/channellineups/Hicksville -H Content-Type:application/json -X POST -d \@build`;
-`curl -H "Authorization: Bearer c49d2ad386d45c41e5c1ca2bbfe531dab7136601d3cc01e3434b97b965118ac2" -ks https://service-mgr.mos.hcvlny.cv.net:8043/v2/channellineups/Hicksville -H Content-Type:application/json -X PUT -d \@build`;
+`curl -H "Authorization: Bearer c49d2ad386d45c41e5c1ca2bbfe531dab7136601d3cc01e3434b97b965118ac2" -ks https://$sm:8043/v2/channellineups/$sitename -H Content-Type:application/json -X POST -d \@build`;
+`curl -H "Authorization: Bearer c49d2ad386d45c41e5c1ca2bbfe531dab7136601d3cc01e3434b97b965118ac2" -ks https://$sm:8043/v2/channellineups/$sitename -H Content-Type:application/json -X PUT -d \@build`;
 unlink("build");
